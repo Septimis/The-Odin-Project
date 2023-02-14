bcf4288c6a4f762e6147eaf55242219df5f72570
septimis <connor.meads@live.com>
2023-02-13 21:32:48 -0700

    Finished Etch-A-Sketch

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -4,20 +4,41 @@ const pixelColor = document.getElementById('pixelColor');
 const isRandomColor = document.getElementById('isRandomColor');
 const gridSize = document.getElementById('gridSize');
 
+gridSize.addEventListener('change', () => createBoard());
+
 //initiate the board
 function createBoard() {
+    clearChildren(gameBoard);
+
     let pixelSize = gameBoard.clientWidth / Number(gridSize.value);
     let numPixels = gameBoard.clientWidth / pixelSize;
 
-    for(let i = 0; i < numPixels; i++) {
+    for(let i = 0; i < numPixels * numPixels; i++) {
         const pixel = document.createElement('div');
         pixel.style.border = 'none';
-        pixel.style.background = pixelColor.value;
-        pixel.style.width = pixelSize;
-        pixel.style.height = pixelSize;
-        //pixel.style.cssText = `border: none; background: ${pixelColor.value}; width: ${pixelSize}; height: ${pixelSize};`;
+        pixel.style.background = `${pixelColor.value}`;
+        pixel.style.width = `${pixelSize}px`;
+        pixel.style.height = `${pixelSize}px`;
+
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.background = (isRandomColor.checked) ? `rgb(${Math.random() * 100 + 1},${Math.random() * 100 + 1},${Math.random() * 100 + 1})` : pixelColor.value;
+        });
+
         gameBoard.appendChild(pixel);
     }
+}
 
-    console.log(`Board will have ${numPixels} pixel(s) of size ${pixelSize}`);
+function clearChildren(parent) {
+    let firstChild = parent.firstElementChild;
+
+    while(firstChild) {
+        firstChild.remove();
+        firstChild = parent.firstElementChild;
+    }
+}
+
+function reset() {
+    pixelColor.value = '#000000';
+    gridSize.value = '16';
+    createBoard();
 }
